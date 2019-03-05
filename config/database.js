bd1259433e73893c9eef81f198d4454019adf6cb
Bharathkns <knsbharath@gmail.com>
2019-03-05 18:59:41 +0530

    second

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-    module.exports = {mongoURI: 'mongodb://bharath:bharath@cluster0-shard-00-00-kmw5c.mongodb.net:27017,cluster0-shard-00-01-kmw5c.mongodb.net:27017,cluster0-shard-00-02-kmw5c.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'}
+    module.exports = {mongoURI: 'mongodb+srv://bharath:bharath@cluster0-kmw5c.mongodb.net/test?retryWrites=true'}
   } else {
     module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
   }
