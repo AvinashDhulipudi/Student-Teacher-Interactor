c05ad40d3da92a5bcb2a6d44259e50b1e487d0b3
Bharathkns <knsbharath@gmail.com>
2019-04-29 02:16:21 +0530

    make it better

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-    module.exports = {mongoURI: 'mongodb://bharath:bharath@ds261155.mlab.com:61155/tsinteract'}
+    module.exports = {mongoURI: 'mongodb://bharathkns:knsb9399@ds261155.mlab.com:61155/tsinteract'}
   } else {
     module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
   }
